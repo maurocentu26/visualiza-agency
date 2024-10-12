18affc3b2b6879ef68acfae0d8ceacbc2fb56fb1
Mauro Centurion <87332552+maurocentu26@users.noreply.github.com>
2024-10-12 20:04:22 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -5,9 +5,15 @@ const form = document.querySelector('form');
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, {
+            // mode: 'no-cors',
             redirect: 'follow',
             method: 'POST', 
             body: new FormData(form),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                'Content-Type': 'application/x-www-form-urlencoded',
+                "Access-Control-Allow-Methods": "POST",
+            },
         })
         .then(response => alert("Thank you! your form is submitted successfully." ))
         .then(() => { window.location.reload(); })
