677010a677c52e74d49758029a4ed6167d49365a
Mauro Centurion <87332552+maurocentu26@users.noreply.github.com>
2024-10-16 00:48:54 -0300

    Update qrgame.js

--- js/qrgame.js
+++ js/qrgame.js
@@ -14,22 +14,32 @@ initGame();
 function initGame() {
     // Initial check for found QR based on URL param
     if (myParam !== null) {
-        const text = "Felicidades, encontraste el QR N° " + myParam;
+        const qrNumber = parseInt(myParam);
+        updateHTMLQr();
+
+        if(getFoundQrs.length === 5) {
+            console.log("Todos los QRs encontrados");
+            showModal("¡Felicidades! Encontraste todos los QRs");
+            return;
+        }
+
+        if(buttons[qrNumber - 1].classList.contains("inactive")) {
+            const text = "Encontraste 1 de 4 QR.\n¡Pero antes debes encontrar los QRs anteriores!\n Al presionar en cada QR obtendrás una pista para encontrar el siguiente.";
+            showModal(text);
+            return;
+        } 
+
+        const text = "Felicidades, encontraste el QR N° " + qrNumber + " y desbloqueaste la pista para el siguiente QR.";
         showModal(text);
-        updateFoundQrs(parseInt(myParam)); // Convert URL param to integer
-        buttons[myParam - 1].classList.add("founded");
-    }
-
-    const foundQrs = getFoundQrs();
-    foundQrs.forEach(qrNumber => {
+        updateFoundQrs(parseInt(qrNumber));
         buttons[qrNumber - 1].classList.add("founded");
-    });
-
-    if(foundQrs.length === 4) {
-        console.log("Todos los QRs encontrados");
-        showModal("¡Felicidades! Encontraste todos los QRs");
-        buttons[4].classList.remove("inactive");
+        console.log(buttons[qrNumber]);
+        if(qrNumber <= 4) {
+            buttons[qrNumber].classList.remove("inactive");
+        }
     }
+
+    updateHTMLQr();
 }
 
 // Function to retrieve found QR data from local storage
@@ -38,38 +48,49 @@ function getFoundQrs() {
     return storedData ? JSON.parse(storedData) : [];
 }
 
+function updateHTMLQr() {
+    const foundQrs = getFoundQrs();
+    foundQrs.forEach(qrNumber => {
+        if(qrNumber <= 4) {
+            buttons[qrNumber - 1].classList.add("founded");
+            buttons[qrNumber].textContent = qrNumber + 1;
+            buttons[qrNumber].classList.remove("inactive");
+        } else buttons[qrNumber - 1].classList.add("founded");
+    });
+}
+
 // Function to update local storage with new found QR data
 function updateFoundQrs(qrNumber) {
-  const foundQrs = getFoundQrs();
-  
-  if (!foundQrs.includes(qrNumber)) {
+    const foundQrs = getFoundQrs();
+
+    if (!foundQrs.includes(qrNumber)) {
     foundQrs.push(qrNumber);
     localStorage.setItem(storageKey, JSON.stringify(foundQrs));
-  }
+    }
 }
 
 // Function to check if a QR is already found
 function isFound(qrNumber) {
-  const foundQrs = getFoundQrs();
-  return foundQrs.includes(qrNumber);
+    const foundQrs = getFoundQrs();
+    return foundQrs.includes(qrNumber);
 }
 
 const textArr = [
-  "Pista para el QR N° 1",
-  "Pista para el QR N° 2",
-  "Pista para el QR N° 3",
-  "Pista para el QR N° 4",
-  "Pista para el QR N° 5"
+    "Pista para el QR N° 1",
+    "Pista para el QR N° 2",
+    "Pista para el QR N° 3",
+    "Pista para el QR N° 4",
+    "Pista para el QR N° 5"
 ];
 
 buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
+    button.addEventListener('click', () => {
     if (isFound(index + 1) || button.classList.contains("inactive")) {
-      return; // Don't show modal for already found QR
+        return; // Don't show modal for already found QR
     }
     showModal(textArr[index]);
     // updateFoundQrs(index + 1); // Store the QR number found (index + 1)
-  });
+    });
 });
 
 span.onclick = function() {
